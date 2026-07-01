2b4641ec5632673fb02feb8cbbecbc1645936737
yuriinter10-commits <yuri.inter10@gmail.com>
2026-06-30 21:35:35 -0300

    finalizando Sobre_nos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# blog/models.py
+from django.db import models
+
+class Categoria(models.Model):
+    # Coluna de texto curto (máximo 100 caracteres)
+    nome = models.CharField(max_length=100)
+
+    # Como o objeto deve ser lido por humanos no painel
+    def __str__(self):
+        return self.nome
+
+# 2. Tabela de Artigos
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    # Texto longo, sem limite de caracteres
+    conteudo = models.TextField()
+    # Preenche com a data/hora exata do cadastro
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    # Relação com a Categoria (ForeignKey = Chave Estrangeira)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+    autor = models.CharField(max_length=100, default="Admin")
+
+    def __str__(self):
+        return self.titulo
