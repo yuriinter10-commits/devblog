4d373fd2492bb7f4d9a00f1322704150d69f9835
yuriinter10-commits <yuri.inter10@gmail.com>
2026-07-03 21:43:36 -0300

    terminar sobre

--- core/settings.py
+++ core/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'devblog_db',
+        'USER': 'root',
+        'PASSWORD': 'senacrs',
+        'HOST': 'localhost',
+        'PORT': '3307',   
+        }
 }
 
 
